66805cc725fe02e5e1786d5896acefe3044c7ab9
Yaroslav <tvoibaget@mail.ru>
2023-11-03 12:05:34 +0300

    prettier fix, added prettier and tests checks

--- config/rollup.prod.config.js
+++ config/rollup.prod.config.js
@@ -6,14 +6,14 @@ import replace from '@rollup/plugin-replace';
 const prodPlugins = [...plugins];
 
 prodPlugins.unshift(
-    replace({
-        preventAssignment: true,
-        'process.env.NODE_ENV': JSON.stringify('production'),
-    })
+  replace({
+    preventAssignment: true,
+    'process.env.NODE_ENV': JSON.stringify('production'),
+  })
 );
 const prodRollup = {
-    input,
-    output,
-    plugins: prodPlugins,
+  input,
+  output,
+  plugins: prodPlugins,
 };
 export default prodRollup;
