b660dadf2b764d40717461e7e89744326328a9b2
Yaroslav <tvoibaget@mail.ru>
2023-11-03 16:24:27 +0300

    fix: remove withTodos

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import { Calendar } from 'components/Calendar';
-import { CalendarAllProps } from 'components/Calendar';
 import { withInput } from 'components/HOCs/withInput';
 import { withInputs } from 'components/HOCs/withInputs';
-import React, { FC } from 'react';
+
 class CalendarService {
   static getDefaultCalendar() {
     return Calendar;
@@ -12,11 +11,6 @@ class CalendarService {
   }
   static withInputs() {
     return withInputs(Calendar);
-  }
-  static withTodos(Component: FC<CalendarAllProps>) {
-    return (props: FC<CalendarAllProps>) => {
-      <Component {...props} withTodos={true} />;
-    };
   }
 }
 
